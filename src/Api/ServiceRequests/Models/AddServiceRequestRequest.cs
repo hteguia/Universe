1e09a3287dc2b8a1b056f24a0070e466e26bd42a
Hervé TEGUIA <tkh.tegus@gmail.com>
2024-08-07 10:01:26 +0400

    up

--- src/Api/ServiceRequests/Models/AddServiceRequestRequest.cs
+++ src/Api/ServiceRequests/Models/AddServiceRequestRequest.cs
@@ -4,6 +4,6 @@ public class AddServiceRequestRequest
 {
     public string Name { get; init; }
     public IFormFile Content { get; init; }
-    public long DocumentTypeId { get; set; }
+    public required long DocumentTypeId { get; set; }
     public string DeadLine { get; set; }
 }
